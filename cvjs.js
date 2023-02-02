568964e7ff353fd39eba641b556d5a7a232ea6c8
Ignacio de las Navas <salhem1313@gmail.com>
2023-02-01 23:35:02 -0300

    CV curso

---
+++ cvjs.js
@@ -0,0 +1,10 @@
+
+document.getElementById('boton_si').addEventListener('click', function() {
+    
+    document.getElementById("demo").innerHTML = "Genial =D!!!";
+})
+
+document.getElementById('boton_no').addEventListener('click', function() {
+        
+    document.getElementById("demo").innerHTML = "Debo estudiar más =(";
+})
